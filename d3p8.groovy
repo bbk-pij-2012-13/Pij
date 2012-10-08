a5c1b7e71f5802ef2f576fdcc5d1f4deb98c3606
unknown <ireigm01@cspc042.dcs.bbk.ac.uk>
2012-10-08 15:29:16 +0100

    upload

---
+++ d3p8.groovy
@@ -0,0 +1,9 @@
+println "Introduce a text"
+String str= System.console().readLine()
+
+boolean check=false
+for (int i=0;i<str.length();i++){
+print str.charAt(i)
+}
+for (int j=str.length()-1;j>-1;j--){
+print str.charAt(j)}
